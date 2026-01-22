70eba1101de7fa5adaf6427671791cc212d52d5e
Tamara <vovktamars@gmail.com>
2026-01-22 19:22:08 +0300

    add changes to Driver and update pom.xml

--- Dns-shop.ru/src/main/java/dns/driver/Driver.java
+++ Dns-shop.ru/src/main/java/dns/driver/Driver.java
@@ -13,14 +13,7 @@ public class Driver {
     ;
     public static WebDriver getDriver() {
         if (driver == null) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--disable-blink-features=AutomationControlled");
-            options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
-            options.setExperimentalOption("useAutomationExtension", false);
-
-            options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
-
-            driver = new ChromeDriver(options);
+            driver = new ChromeDriver();
             driver.manage().window().maximize();
         }
         return driver;
